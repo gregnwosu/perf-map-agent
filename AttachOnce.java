22756077d4fefafbde9daea01b8903049f130602
Greg Nwosu <gregn@blinkbox.com>
2015-01-05 11:19:07 +0000

    patch

--- AttachOnce.java
+++ AttachOnce.java
@@ -28,15 +28,16 @@ import java.lang.management.ManagementFactory;
 public class AttachOnce {
     public static void main(String[] args) throws Exception {
         String pid = args[0];
+        String path = args[1]
         String options = "";
         if (args.length > 1) options = args[1];
-        loadAgent(pid, options);
+        loadAgent(pid, options, path);
     }
 
-    static void loadAgent(String pid, String options) throws Exception {
+    static void loadAgent(String pid, String options,String path) throws Exception {
         VirtualMachine vm = VirtualMachine.attach(pid);
         try {
-            vm.loadAgentPath(new File("./libperfmap.so").getAbsolutePath(), options);
+            vm.loadAgentPath(new File(path).getAbsolutePath(), options);
         } catch(com.sun.tools.attach.AgentInitializationException e) {
             // rethrow all but the expected exception
             if (!e.getMessage().equals("Agent_OnAttach failed")) throw e;
